0b35cda2cc94a39988348dedcf051aa0d1b6d638
Josh Merlino <joshua.merlino@gmail.com>
2024-11-14 20:10:42 -0500

    Add CI workflow for version bump, linting, and Docker image build

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+// Start an http server on port 8000
+
+import { serve } from "bun";
+
+serve({
+	port: 8000,
+	fetch(req) {
+		const url = new URL(req.url);
+		if (url.pathname === "/") return new Response("Home page");
+		return new Response("404!");
+	},
+});
